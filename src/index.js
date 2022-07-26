abade80a8f4f2f2cf11fe975a62c56ce744680a7
Minimise <jackson.whiteau@gmail.com>
2022-07-27 06:03:06 +0800

    Edit

--- src/index.js
+++ src/index.js
@@ -1,63 +1,62 @@
-myTasks = []
+myTasks = [];
 
 const showButton = document.querySelector('#newTask');
-showButton.addEventListener('click', function() {
-    showForm()
+showButton.addEventListener('click', () => {
+  showForm();
 });
 
 function deleteTask() {
-    const task = document.querySelector('.task');
-    task.addEventListener('click', function() {
-        task.style.textDecoration = 'line-through';
-        task.style.color = 'green';
+  document
+    .getElementsByClassName('task')
+    .addEventListener('click', function (event) {
+      if (event.originalTarget.class === 'task') {
+        this.parentElement.removeChild(this);
+      }
     });
 }
-
 const formInfo = document.querySelector('#submit');
-formInfo.addEventListener('click', function() {
-    console.log('hi')
-    getFormInfo()
-    showForm()
+formInfo.addEventListener('click', () => {
+  getFormInfo();
+  showForm();
 });
 
 function showForm() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+  const popup = document.getElementById('myPopup');
+  popup.classList.toggle('show');
 }
 
 class myTask {
-    constructor(title,desc,dueDate,priority,created) {
-
-    this.title = title
-    this.desc = desc
-    this.dueDate = dueDate
-    this.priority = priority
-    this.created = created
-    this.info = function(){
-        return [title, desc, dueDate, priority, created]
-    }
-}
+  constructor(title, desc, dueDate, priority, created) {
+    this.title = title;
+    this.desc = desc;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.created = created;
+    this.info = function () {
+      return [title, desc, dueDate, priority, created];
+    };
+  }
 }
 
 function getFormInfo() {
-    let title = document.getElementById("title").value;
-    let desc = document.getElementById("description").value;
-    let dueDate = document.getElementById("dueDate").value;
-    let important = document.getElementById("important").checked;
-    let created = false;
-    myTasks.push(new myTask (title, desc, dueDate, important, created))
-    addTask()
+  const title = document.getElementById('title').value;
+  const desc = document.getElementById('description').value;
+  const dueDate = document.getElementById('dueDate').value;
+  const important = document.getElementById('important').checked;
+  const created = false;
+  myTasks.push(new myTask(title, desc, dueDate, important, created));
+  addTask();
 }
 
 function addTask() {
-    for (const item of myTasks) {
-        if (item.created == false) {
-            let div = document.createElement('div');
-            div.classList.add("task");
-            div.innerHTML += item.title + ': '  + item.desc + ' by ' + item.dueDate
-            document.getElementById("tasks").appendChild(div);
-            item.created = true;
-        }
-        deleteTask();
+  for (const item of myTasks) {
+    if (item.created == false) {
+      const div = document.createElement('div');
+      div.classList.add('task');
+      div.innerHTML += `${item.title}: ${item.desc} by ${item.dueDate}`;
+      document.getElementById('tasks').appendChild(div);
+      item.created = true;
     }
-}   
+    deleteTask();
+  }
+}
